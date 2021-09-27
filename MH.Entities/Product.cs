f2528f2ad2fb6f10a26940e8e5acf2f054f4bdd9
kader <akader080@gmail.com>
2021-09-27 14:58:39 +0600

    A1.1 Category and Product enity add with relation

---
+++ MH.Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH.Entities
+{
+    public class Product
+    {
+        public int PID { get; set; }
+        public string Name { get; set; }
+        public decimal MrpPrice { get; set; }
+        public int PackSize { get; set; }
+        public string Description { get; set; }
+
+
+        public Category Category { get; set; }
+    }
+}
