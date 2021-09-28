6ac59ac1206c601c6b1f57ffff75019a9291c13e
kader <akader080@gmail.com>
2021-09-29 00:35:50 +0600

    A2.2 MH.Service project add, CRUD perform try in Category

---
+++ MH.Services/CategoriesService.cs
@@ -0,0 +1,39 @@
+﻿using MH.Database;
+using MH.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH.Services
+{
+    public class CategoriesService
+    {
+        #region Singleton
+        public static CategoriesService Instance
+        {
+            get
+            {
+                if (instance == null) instance = new CategoriesService();
+                return instance;
+            }
+        }
+        private static CategoriesService instance { get; set; }
+        private CategoriesService()
+        {
+
+        }
+        #endregion
+        public void SaveCategory(Category category)
+        {
+            using (var context = new MHDbContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
