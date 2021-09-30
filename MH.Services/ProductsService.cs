42b2fa6b323e77a0e8e201120c29872efa0cd80d
kader <akader080@gmail.com>
2021-10-01 01:14:17 +0600

    A3/18,28partialView(),35search,45 JQuery +Ajax ,1.11 LinQ   all use for CRUD in Product

---
+++ MH.Services/ProductsService.cs
@@ -0,0 +1,76 @@
+﻿using MH.Database;
+using MH.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH.Services
+{
+    public class ProductsService
+    {
+        #region Singleton
+        public static ProductsService Instance
+        {
+            get
+            {
+                if (instance == null) instance = new ProductsService();
+                return instance;
+            }
+        }
+        private static ProductsService instance { get; set; }
+        private ProductsService()
+        {
+
+        }
+        #endregion
+        public void SaveProduct(Product product)
+        {
+            using (var context = new MHDbContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            using (var context = new MHDbContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new MHDbContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new MHDbContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new MHDbContext())
+            {
+                var product = context.Products.Where(x => x.PID == ID).FirstOrDefault();
+                // var category = context.Categories.Where(x => x.CID == ID).Include(x => x.Products).FirstOrDefault();
+
+                //context.Products.RemoveRange(category.Products);  //first delete products of this category
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ MH.Web/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using MH.Entities;
+using MH.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MH.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable(string search)
+        //public ActionResult ProductTable(string search, int? pageNo)
+        {
+            //var pageSize = 5;// ConfigurationsService.Instance.PageSize();
+
+            //ProductSearchViewModel model = new ProductSearchViewModel();
+            //model.SearchTerm = search;
+            //pageNo = pageNo.HasValue ? pageNo.Value > 0 ? pageNo.Value : 1 : 1;
+
+            //var countProduct = ProductsService.Instance.GetProductsCount(search);
+            //model.Products = ProductsService.Instance.GetProducts(search, pageNo.Value, pageSize);
+            var products = ProductsService.Instance.GetAllProducts();
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(x => x.Name != null && x.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+
+            //model.Pager = new Pager(countProduct, pageNo, pageSize);
+            //return PartialView(model);
+            return PartialView(products);
+        }
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ProductsService.Instance.SaveProduct(product);
+                return RedirectToAction("ProductTable");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
